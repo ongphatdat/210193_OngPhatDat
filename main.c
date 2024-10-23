a57259b11ee26bb1dbfa4f7dd9e4a87ee96f1679
Ông Phát Đạt <datst2908@gmail.com>
2024-10-23 10:01:22 +0700

    cau10

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+// Khai báo prototype của các hàm từ shellc1.c và shellc2.c
+void print_shellc1();
+void print_shellc2();
+
+int main() {
+    print_shellc1();
+    print_shellc2();
+    return 0;
+}
